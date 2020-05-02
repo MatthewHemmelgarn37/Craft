5e820203307c77df7a083ca8ddf87c277027f4de
Brandon Eyongherok <brandoneyongherok@Brandons-Air.lan1>
2020-05-02 06:06:13 -0400

    change the color of the trees to yellow

--- src/item.h
+++ src/item.h
@@ -32,7 +32,7 @@
 #define COLOR_04 36
 #define COLOR_05 37
 #define COLOR_06 38
-#define COLOR_07 53
+#define COLOR_07 39
 #define COLOR_08 40
 #define COLOR_09 41
 #define COLOR_10 42
@@ -40,7 +40,7 @@
 #define COLOR_12 44
 #define COLOR_13 45
 #define COLOR_14 46
-#define COLOR_15 47
+#define COLOR_15 53
 #define COLOR_16 48
 #define COLOR_17 49
 #define COLOR_18 50
